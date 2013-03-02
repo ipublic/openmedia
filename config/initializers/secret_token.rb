b317b686ebc42dc0a707898dd3578b789b7b4a43
Dan Thomas <dan.thomas@ipublic.org>
2013-03-02 13:44:36 -0500

    initial commit
    includes mongoid and twitter bootstrap inits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openmedia::Application.config.secret_token = '78fbfd59d9941e7d38bd20ae534799819e64269483f8a9fefa87b1ff8d68166b2d1987c4e38a4196baf3f918c1d9210786b5913be766d32cdcb654d32e2ec8b3'
