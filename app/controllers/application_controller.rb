bd3c71f13c833e41dd06687bd67b87fb52384448
Dan Thomas <dan.thomas@ipublic.org>
2013-03-02 15:52:42 -0500

    Add: site layout template and landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # Set landing page following successful login
+  # def after_sign_in_path_for(resource)
+    # dashboards_index_path
+  # end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Openmedia::Application.routes.draw do
   
   
   devise_for :users
+
+  root :to => "home#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
