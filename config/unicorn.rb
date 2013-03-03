b8b2beb7bae02c0b78ec4b82f363a274f76150b4
Dan Thomas <dan.thomas@ipublic.org>
2013-03-03 11:07:35 -0500

    Add: nginx and unicorn config files

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deployer/apps/ap/current"
+pid "/home/deployer/apps/ap/shared/pids/unicorn.pid"
+stderr_path "/home/deployer/apps/ap/shared/log/unicorn.log"
+stdout_path "/home/deployer/apps/ap/shared/log/unicorn.log"
+
+listen "/tmp/unicorn.ap.sock"
+worker_processes 2
+timeout 30
